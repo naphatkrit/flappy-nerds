cf4d846beef69e124737f913e501f843d2216311
Naphat Sanguansin <naphat.krit@gmail.com>
2016-05-03 16:10:14 -0400

    setup camera in the right coordinate system. need to refactor

--- js/main.ts
+++ js/main.ts
@@ -2,10 +2,12 @@ var SCREEN_WIDTH = window.innerWidth;
 var SCREEN_HEIGHT = window.innerHeight;
 var aspect = SCREEN_WIDTH / SCREEN_HEIGHT;
 var container: HTMLDivElement, stats;
-var scene: THREE.Scene, renderer: THREE.WebGLRenderer, mesh: THREE.Mesh;
+var camera, scene: THREE.Scene, renderer: THREE.WebGLRenderer, bird: THREE.Mesh;
 var cameraRig: THREE.Group, activeCamera: THREE.Camera;
-var cameraPerspective: THREE.PerspectiveCamera;
+var cameraPerspective: THREE.PerspectiveCamera, cameraOrtho: THREE.OrthographicCamera;
+var cameraPerspectiveHelper;
 var frustumSize = 600;
+var activeHelper;
 
 window.onload = function() {
     init();
@@ -16,38 +18,32 @@ function init() {
     container = document.createElement('div');
     document.body.appendChild(container);
     scene = new THREE.Scene();
+
+    camera = new THREE.PerspectiveCamera( 50, 0.5 * aspect, 1, 10000 );
+	camera.position.z = 2500;
+
     //
-    cameraPerspective = new THREE.PerspectiveCamera(50, aspect, 150, 1000);
+    cameraPerspective = new THREE.PerspectiveCamera(50, 0.5 * aspect, 150, 1000);
+    cameraPerspectiveHelper = new THREE.CameraHelper( cameraPerspective );
+	scene.add( cameraPerspectiveHelper );
     //
     activeCamera = cameraPerspective;
+    activeHelper = cameraPerspectiveHelper;
     // counteract different front orientation of cameras vs rig
     cameraPerspective.rotation.y = Math.PI;
+    cameraPerspective.position.x = 0;
+    cameraPerspective.position.y = 0;
+    cameraPerspective.position.z = 1000;
+    cameraPerspective.lookAt(new THREE.Vector3(0, 0, -1));
+
     cameraRig = new THREE.Group();
     cameraRig.add(cameraPerspective);
     scene.add(cameraRig);
-    // white ball
-    mesh = new THREE.Mesh(
+    bird = new THREE.Mesh(
         new THREE.SphereBufferGeometry(100, 16, 8),
         new THREE.MeshBasicMaterial({ color: 0xffffff, wireframe: true })
     );
-    scene.add(mesh);
-
-    // green ball
-    var mesh2 = new THREE.Mesh(
-        new THREE.SphereBufferGeometry(50, 16, 8),
-        new THREE.MeshBasicMaterial({ color: 0x00ff00, wireframe: true })
-    );
-    mesh2.position.y = 150;
-    mesh.add(mesh2);
-
-    // inner blue ball
-    var mesh3 = new THREE.Mesh(
-        new THREE.SphereBufferGeometry(5, 16, 8),
-        new THREE.MeshBasicMaterial({ color: 0x0000ff, wireframe: true })
-    );
-    mesh3.position.z = 150;
-    cameraRig.add(mesh3);
-
+    scene.add(bird);
 
     var geometry = new THREE.Geometry();
     for (var i = 0; i < 10000; i++) {
@@ -96,20 +92,19 @@ function animate() {
     render();
     stats.update();
 }
+var count = 0;
 function render() {
-    var r = Date.now() * 0.0005;
-    mesh.position.x = 700 * Math.cos(r);
-    mesh.position.z = 700 * Math.sin(r);
-    mesh.position.y = 700 * Math.sin(r);
-    mesh.children[0].position.x = 70 * Math.cos(2 * r);
-    mesh.children[0].position.z = 70 * Math.sin(r);
-    if (activeCamera === cameraPerspective) {
-        cameraPerspective.fov = 35 + 30 * Math.sin(0.5 * r);
-        cameraPerspective.far = mesh.position.length();
-        cameraPerspective.updateProjectionMatrix();
-    }
-    cameraRig.lookAt(mesh.position);
+    count += 1;
+    bird.position.x = count;
+    bird.position.y = 0;
+    bird.position.z = 0;
+    cameraRig.position.x = count;
+    console.log(cameraRig.position);
     renderer.clear();
-    renderer.setViewport(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT);
-    renderer.render(scene, activeCamera);
+    activeHelper.visible = false;
+	renderer.setViewport( 0, 0, SCREEN_WIDTH/2, SCREEN_HEIGHT );
+	renderer.render( scene, activeCamera );
+	activeHelper.visible = true;
+	renderer.setViewport( SCREEN_WIDTH/2, 0, SCREEN_WIDTH/2, SCREEN_HEIGHT );
+	renderer.render( scene, camera );
 }
