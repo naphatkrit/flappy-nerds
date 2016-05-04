eb0232bea2eee7c7d018d4a59b967f1d04f88885
Xiaohua Liang <lxhfirenking@hotmail.com>
2016-05-04 16:01:41 -0400

    most basic autopilot

---
+++ js/autopilot.ts
@@ -0,0 +1,17 @@
+abstract class autopilot {
+	private static thresh:number = 0.0
+
+	private static getTargetHeight(box:THREE.Box3):number{
+		var ymin:number = box.min.y
+		var ymax:number = box.max.y
+
+		return (ymax - ymin) / 2.0
+	}
+
+	public static control(bird:Bird, targetBox:THREE.Box3) {
+		var birdPosY: number = bird.mesh.position.y
+		if ((birdPosY - this.getTargetHeight(targetBox)) < this.thresh) {
+			bird.setNeedsJump() 
+		}
+	}
+}
