6aa1abcf7ed91458c923ad561abce45f435766e4
Naphat Sanguansin <naphat.krit@gmail.com>
2016-05-04 15:47:19 -0400

    tweaked the physics so it's easier to play

--- js/Config.ts
+++ js/Config.ts
@@ -1,8 +1,8 @@
 abstract class Config {
     public static VELOCITY = new THREE.Vector3(100, 0, 0);
     public static DEBUG = true;
-    public static GRAVITY = new THREE.Vector3(0, -3000, 0);
-    public static JUMP_VELOCITY = new THREE.Vector3(100, 1000, 0);
+    public static GRAVITY = new THREE.Vector3(0, -2000, 0);
+    public static JUMP_VELOCITY = new THREE.Vector3(100, 600, 0);
     public static MAX_Y = 400;
     public static MIN_Y = -400;
     public static OBSTACLE_INTERVAL = 4500;
