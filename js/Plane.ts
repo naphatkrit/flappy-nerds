030283268ef3e8faf2e3e9a01ec57d7027c131a5
Naphat Sanguansin <naphat.krit@gmail.com>
2016-05-05 08:42:29 -0400

    Plane now has a moving texture

--- js/Plane.ts
+++ js/Plane.ts
@@ -3,13 +3,17 @@ class Plane implements I3DObject {
     private _mesh: THREE.Mesh;
     private _velocity: THREE.Vector3;
     private _collisionEffect: CollisionEffect;
+    private _texture: THREE.Texture;
 
     constructor(scene: THREE.Scene, y: number, velocity: THREE.Vector3, collisionEffect: CollisionEffect) {
-        //var texture = new THREE.TextureLoader().load( "js/textures/water.jpg" );
+        var texture = new THREE.TextureLoader().load( "js/textures/water.jpg" );
+        texture.wrapS = THREE.MirroredRepeatWrapping;
+        texture.wrapT = THREE.MirroredRepeatWrapping;
         this._mesh = new THREE.Mesh(
             new THREE.PlaneGeometry(window.innerWidth, window.innerWidth, 1, 1),
-            new THREE.MeshBasicMaterial({ color: 0xEB7D07, wireframe: false})
+            new THREE.MeshBasicMaterial({ map: texture, wireframe: false})
         );
+        this._texture = texture;
         this._mesh.translateY(y);
         this._mesh.rotateX((y > 0 ? 1 : -1) * Math.PI / 2.0);
         // force the transforms to take effect right away, important for collision detection
@@ -29,6 +33,7 @@ class Plane implements I3DObject {
 
     public update(deltaSeconds: number) {
         this._mesh.position.addScaledVector(this._velocity, deltaSeconds);
+        this._texture.offset.x = (this._texture.offset.x + this._velocity.x * deltaSeconds / this._texture.image.width);
     }
 
     public stop() {
